bb985a24dfd83aedc0cdce648ebcecf953a5481b
random <whatever@meh.com>
2023-02-24 14:28:38 +0100

    Update fmhy-search.py

--- fmhy-search.py
+++ fmhy-search.py
@@ -7,7 +7,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Wiki': 'https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/index/',
         'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# This is a header. This is an *extremely* cool app!"
     }
