20a4bd3c9115bb7017391ebe08dc8eba036c4ec2
random <whatever@meh.com>
2023-02-24 14:21:07 +0100

    Update fmhy-search.py

--- fmhy-search.py
+++ fmhy-search.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="FMHY Search", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="FMHY Search", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items={'Wiki': 'https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/index/', 'Raw': "https://raw.githubusercontent.com/nbats/FMHYedit/main/single-page", 'About': "https://github.com/Rust1667/a-FMHY-search-engine"})
 
 #st.image("https://i.imgur.com/s9abZgP.png", width=60)
 st.title("Search FMHY")
