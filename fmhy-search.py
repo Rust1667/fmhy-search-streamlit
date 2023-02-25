703c01405c91ec234a702877241a04f31c44ce57
random <whatever@meh.com>
2023-02-25 11:48:18 +0100

    Update fmhy-search.py

--- fmhy-search.py
+++ fmhy-search.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="FMHY Search",
-    page_icon="https://www.fmhy.ml/assets/logo.png",
+    page_icon="https://i.imgur.com/s9abZgP.png",
     layout="centered",
     initial_sidebar_state="collapsed",
     menu_items={
