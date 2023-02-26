4a006b2cedc850c0c799ff441fd28530313422a7
random <whatever@meh.com>
2023-02-26 22:08:52 +0100

    Update fmhy-search.py

--- fmhy-search.py
+++ fmhy-search.py
@@ -170,11 +170,25 @@ def doASearch(searchInput):
 lineList = getAllLines()
 
 
+credentialsDictEnv = {
+  "type": st.secrets.type,
+  "project_id": st.secrets.project_id,
+  "private_key_id": st.secrets.private_key_id,
+  "private_key": st.secrets.private_key,
+  "client_email": st.secrets.client_email,
+  "client_id": st.secrets.client_id,
+  "auth_uri": st.secrets.auth_uri,
+  "token_uri": st.secrets.token_uri,
+  "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
+  "client_x509_cert_url": st.secrets.client_x509_cert_url
+}
+
+
 ## Streamlit code
 if st.button("Search"):
     doASearch(queryInput)
 
     try:
-        logToGoogleSheet(queryInput, st.secrets["CREDENTIALS_DICT"])
+        logToGoogleSheet(queryInput, credentialsDictEnv)
     except Exception as e: print(e)
 
