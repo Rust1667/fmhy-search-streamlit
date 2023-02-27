eb4642a8f3390060d1dccc0d25c030e1f6ecddff
random <whatever@meh.com>
2023-02-27 13:24:01 +0100

    Update fmhy-search.py

--- fmhy-search.py
+++ fmhy-search.py
@@ -65,21 +65,20 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import json
 
-credentials = {
-  "type": st.secrets.type,
-  "project_id": st.secrets.project_id,
-  "private_key_id": st.secrets.private_key_id,
-  "private_key": st.secrets.private_key,
-  "client_email": st.secrets.client_email,
-  "client_id": st.secrets.client_id,
-  "auth_uri": st.secrets.auth_uri,
-  "token_uri": st.secrets.token_uri,
-  "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
-  "client_x509_cert_url": st.secrets.client_x509_cert_url
-}
-
 @st.cache_resource
 def getAuthorizedGoogleSheet():
+    credentials = {
+      "type": st.secrets.type,
+      "project_id": st.secrets.project_id,
+      "private_key_id": st.secrets.private_key_id,
+      "private_key": st.secrets.private_key,
+      "client_email": st.secrets.client_email,
+      "client_id": st.secrets.client_id,
+      "auth_uri": st.secrets.auth_uri,
+      "token_uri": st.secrets.token_uri,
+      "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
+      "client_x509_cert_url": st.secrets.client_x509_cert_url
+    }
     scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
